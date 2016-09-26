7ce86daaeddab63e7376d115f5a6693d878d7c9d
Glenn <glynester@hotmail.com>
2016-09-26 18:57:28 +0000

    handles multiples.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,15 @@
 def fizzbuzz(number)
-    if number == 3
-        return "fizz"
-    elsif number == 5
-        return "buzz"
-    elsif number == 15
+    if number%3 == 0 && number%5 == 0
         return "fizzbuzz"
+    elsif number%3 == 0
+        return "fizz"
+    elsif number%5 == 0
+        return "buzz"
     end
 end
 
+puts fizzbuzz(4)
 puts fizzbuzz(3)
 puts fizzbuzz(5)
 puts fizzbuzz(15)
+puts fizzbuzz(6)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -27,3 +27,9 @@ describe "fizzbuzz" do
       expect(fizzbuzz(6)).to eq "fizz"
     end
 end
+
+describe "fizzbuzz" do
+    it 'returns "4" when passed 4' do
+        expect (fizzbuzz(4)).to eq "4"
+    end
+end
