4f6af552825e686656cbad65f67df3f5194be315
michkles <michkles@gmail.com>
2016-09-26 19:44:25 +0100

    new tests added

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,8 +3,11 @@ def fizzbuzz(number)
         return "fizz"
     elsif number == 5
         return "buzz"
+    elsif number == 15
+        return "fizzbuzz"
     end
 end
 
 puts fizzbuzz(3)
 puts fizzbuzz(5)
+puts fizzbuzz(15)
